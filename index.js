ec1fec146cdbe1f7b1efcd5daa12951e01fd453b
Seunggyu <seunggyul14@icloud.com>
2022-11-28 09:06:18 -0800

    commit tree f930ba5d8e615b7ae99d7c6674398df2a3e6f7d9

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
-// code your solution here
+// when its roller-skate then return "this saturday, i want to roller-skate' 
+// when its bathe my dog retirn "this saturday, i want to bathe my dog"
+// anything else is "this saturday, i want to roller-skate"
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(punctuation){
+    return function (statement) {
+        return `You are ${punctuation}${statement}${punctuation}!`
+    }
+}
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
